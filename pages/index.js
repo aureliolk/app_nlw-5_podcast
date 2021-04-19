b5419ac002923234fcf2d6457adaadc208ae27e3
Aurélio Castro <aurelio.cos@outlook.com>
2021-04-19 12:50:31 -0300

    Fix Erro Wirith

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Home() {
 
   return (
     <>
-      <h1>App NWL#05</h1>
-      <h2>Você concluiu a Aula?</h2>
+      <h1>App NLW#05</h1>
+      <h2>Você concluiu a 1ª Aula?</h2>
       <Button title="Sim"/>
       <Button title="Não"/>
     </>
